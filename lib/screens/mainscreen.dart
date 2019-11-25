0e9cab32617ec0adbd11a2b7cc5c7614b755b564
dgonz234 <dag81099@gmail.com>
2019-11-25 14:44:08 -0600

    initial

---
+++ lib/screens/mainscreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/blocs/appbloc.dart';
+import 'package:provider/provider.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    AppBloc bloc = Provider.of<AppBloc>(context);
+    return Scaffold(appBar: AppBar(title: Text("ScanAwake"),),
+    body: 
+          Text("empty")
+    );
+  }
+}

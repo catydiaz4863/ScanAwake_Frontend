228120885e0587fe79a529a13a8755619c6ca08c
dgonz234 <dag81099@gmail.com>
2019-11-27 12:09:58 -0600

    - added test sound player & android_alarm_manager

--- lib/screens/mainscreen.dart
+++ lib/screens/mainscreen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:scanawake/blocs/appbloc.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
+import 'package:android_alarm_manager/android_alarm_manager.dart';
+
 
 class MainScreen extends StatefulWidget {
   @override
@@ -11,10 +14,16 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     AppBloc bloc = Provider.of<AppBloc>(context);
-
-    return Scaffold(appBar: AppBar(title: Text("ScanAwake"),),
-    body:
-          Text("empty")
-    );
+    DateTime current;
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("ScanAwake"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text("empty screen"),
+            
+          ],
+        ));
   }
 }
